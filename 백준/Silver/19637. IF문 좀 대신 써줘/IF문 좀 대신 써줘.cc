56d8b29524c003daafc670b58bf7c71e4238968e
seobii <63391974+seobyy@users.noreply.github.com>
2023-03-28 19:25:18 +0900

    [Silver III] Title: IF문 좀 대신 써줘, Time: 84 ms, Memory: 9948 KB -BaekjoonHub

--- 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.cc
+++ 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.cc
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-#define fast_io() ios_base::sync_with_stdio(false);cout.tie(NULL);cin.tie(NULL);
+#define fast_io() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 
 int main() {
     fast_io();
