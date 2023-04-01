ead982da20f9795b015c6f90c6eb66c188571e9b
seobii <63391974+seobyy@users.noreply.github.com>
2023-04-01 12:23:47 +0900

    [Silver III] Title: 비슷한 단어, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/2607. 비슷한 단어/비슷한 단어.c
+++ 백준/Silver/2607. 비슷한 단어/비슷한 단어.c
@@ -1,59 +1,32 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main() {
-    int N, A1[26] = {0}, cnt = 0;
-    char word1[11], word[11];
-    scanf("%d", &N);
-    scanf("%s", word1);
-    int len1 = strlen(word1);
-    for (int i = 0; i < len1; ++i) {
-        A1[word1[i] - 'A']++;
-    }
+    int N, cnt = 0, A[26] = {0}; 
+    char a[11], b[11];
+    scanf("%d %s", &N, a);
+    int la = strlen(a);
+    for (int i = 0; i < la; ++i) 
+        A[a[i] - 'A']++;
     for (int i = 1; i < N; ++i) {
-        scanf("%s", word);
-        int A[26];
-        memcpy(A, A1, sizeof(A1));
-        int len = strlen(word);
-        for (int j = 0; j < len; ++j) {
-            A[word[j] - 'A']--;
-        }
-        int check = 0;
+        int B[26] = {0};
+        scanf("%s", b);
+        int lb = strlen(b);
+        int x = 0;
+        if (abs(la - lb) > 1)
+            continue;
+        for (int j = 0; j < lb; ++j) 
+            B[b[j] - 'A']++;
         for (int j = 0; j < 26; ++j) {
-            if (check == 2)
-                break;
-            if (A[j] > 1 || A[j] < -1) {
-                check = 2;
-                break;
+            int y = abs(A[j] - B[j]);
+            if (y > 1) {
+                x = 3; break;
             }
-            else if (A[j] == 1) {
-                if (check == 1) {
-                    check++;
-                    break;
-                }
-                for (int k = j + 1; k < 26; ++k) {
-                    if (A[k] == -1) {
-                        A[k] = 0;
-                        break;
-                    }
-                }
-                check++; 
-            }
-            else if (A[j] == -1) {
-                if (check == 1) {
-                    check++;
-                    break;
-                }
-                for (int k = j + 1; k < 26; ++k) {
-                    if (A[k] == 1) {
-                        A[k] = 0;
-                        break;
-                    }
-                }
-                check++;
-            }
+            else
+                x += y;
         }
-        if (check != 2)
+        if (x <= 2)
             cnt++;
     }
     printf("%d", cnt);
