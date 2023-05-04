b40fc2f9a3d7c494c39ff33ddee1ebad427532c2
seobii <63391974+seobyy@users.noreply.github.com>
2023-05-04 23:30:58 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2180 ms, Memory: 2292 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#define fastio ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+
+using namespace std;
+
+int main() {
+    fastio;
+    int N, num, cnt[100001] = {0};
+    cin >> N;
+    for (int i = 0; i < N; ++i) {
+        cin >> num;
+        cnt[num]++;
+    }
+    for (int i = 1; i<= 10000; ++i) {
+        if (cnt[i] != 0) {
+            for (int j = 0; j < cnt[i]; ++j)
+                printf("%d\n", i);
+        }
+    }
+    return 0;
+}
