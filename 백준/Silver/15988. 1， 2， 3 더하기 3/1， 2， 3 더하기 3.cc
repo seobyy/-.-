394210a326f0d8d27dd405db799a065bb8a1236a
seobyxx <63391974+seobyy@users.noreply.github.com>
2023-12-01 13:21:28 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 8 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.cc
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.cc
@@ -1,11 +1,11 @@
 #include <iostream>
 #define fastio ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
 #define MAX_N 1'000'000
-#define ll long long
+//#define ll long long
 using namespace std;
 
 const int MOD = 1'000'000'009;
-ll dp[MAX_N + 5];
+int dp[MAX_N + 5];
 
 int main() {
     
@@ -15,7 +15,9 @@ int main() {
     
     dp[1] = 1; dp[2] = 2; dp[3] = 4;
     for (int i = 4; i <= MAX_N; ++i) {
-        dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
+        dp[i] = dp[i - 3] + dp[i - 2];
+        dp[i] %= MOD;
+        dp[i] += dp[i - 1];
         dp[i] %= MOD;
     }
     
