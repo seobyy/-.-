400986901cb108d066fc8917b2dfd4690499cb92
seobyxx <63391974+seobyy@users.noreply.github.com>
2023-12-01 13:20:05 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 8 ms, Memory: 9832 KB -BaekjoonHub

---
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#define fastio ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+#define MAX_N 1'000'000
+#define ll long long
+using namespace std;
+
+const int MOD = 1'000'000'009;
+ll dp[MAX_N + 5];
+
+int main() {
+    
+    fastio;
+    int T;
+    cin >> T;
+    
+    dp[1] = 1; dp[2] = 2; dp[3] = 4;
+    for (int i = 4; i <= MAX_N; ++i) {
+        dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
+        dp[i] %= MOD;
+    }
+    
+    for (int tc = 1; tc <= T; ++tc) {
+        int n;
+        cin >> n;
+        
+        cout << dp[n] % MOD << '\n';
+    }
+    
+    return 0;
+}
